d4b999c8f90847f880f729163567883f653df0df
David Vanderheyden <david.vanderheyden1@outlook.be>
2021-02-02 08:32:17 +0100

    Use IJSInterup invoke static C# methods from JS

--- Blazor101/src/BlazorWasmApp/Client/wwwroot/js/Main.js
+++ Blazor101/src/BlazorWasmApp/Client/wwwroot/js/Main.js
@@ -1,3 +1,10 @@
 ﻿function logToConsole(message) {
     console.log("invoked from C# : " + message);
 }
+
+function dotnetStaticInvocation() {
+    DotNet.invokeMethodAsync("BlazorWasmApp.Client", "GetTotalAdded")
+        .then(result => {
+            console.log("Count triggered from javascript is " + result);
+        });
+}
