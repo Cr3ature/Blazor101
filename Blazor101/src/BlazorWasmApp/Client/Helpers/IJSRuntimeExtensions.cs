15dad30b376a8ff633682bc18b815781fdf3ba48
David Vanderheyden <david.vanderheyden1@outlook.be>
2021-02-02 08:11:47 +0100

    Use IJSRuntime

---
+++ Blazor101/src/BlazorWasmApp/Client/Helpers/IJSRuntimeExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.JSInterop;
+using System.Threading.Tasks;
+
+namespace BlazorWasmApp.Client.Helpers
+{
+    public static class IJSRuntimeExtensions
+    {
+        public static async ValueTask<bool> Confirm(this IJSRuntime js, string message)
+            => await js.InvokeAsync<bool>("confirm", message);
+
+    }
+}
